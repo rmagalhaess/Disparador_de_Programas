a5e03bfa5823bb36febb051d64309265a23685e7
Ricardo de Magalhães Simões <rmagalhaess@hotmail.com>
2017-02-05 20:30:49 -0200

    fontes

---
+++ get_Icon/get_Icon/Form1.Designer.cs
@@ -0,0 +1,98 @@
+﻿namespace get_Icon
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.pnl_Azul = new System.Windows.Forms.Panel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            this.panel1.Controls.Add(this.pnl_Azul);
+            this.panel1.Location = new System.Drawing.Point(27, 40);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(121, 112);
+            this.panel1.TabIndex = 0;
+            // 
+            // pnl_Azul
+            // 
+            this.pnl_Azul.BackColor = System.Drawing.Color.Blue;
+            this.pnl_Azul.Location = new System.Drawing.Point(39, 78);
+            this.pnl_Azul.Name = "pnl_Azul";
+            this.pnl_Azul.Size = new System.Drawing.Size(62, 31);
+            this.pnl_Azul.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(165, 47);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "/\\";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(165, 126);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "\\/";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel pnl_Azul;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ get_Icon/get_Icon/Form1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+
+namespace get_Icon
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int cont = this.pnl_Azul.Top;
+            for( cont=this.pnl_Azul.Top; cont>10; cont--)
+            {
+                this.pnl_Azul.Top = cont;
+                Thread.Sleep(5);
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            int cont = this.pnl_Azul.Top;
+            for( cont=this.pnl_Azul.Top; cont<80; cont++)
+            {
+                this.pnl_Azul.Top = cont;
+                Thread.Sleep(5);
+            }
+        }
+    }
+}
